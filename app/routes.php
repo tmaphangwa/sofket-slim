2f17afbac0d91100497b882b4eb9b27b4f83cd0e
gali <tmaphangwa04@gmail.com>
2025-07-26 14:35:32 +0200

    app route added

--- app/routes.php
+++ app/routes.php
@@ -1,9 +1,29 @@
 <?php
     use Slim\App;
+    use Jenssegers\Blade\Blade;
     use Psr\Http\Message\ServerRequestInterface as Request;
     use Psr\Http\Message\ResponseInterface as Response;
+    
+    function view(Response $response, $template, $with){
+        $cache = __DIR__. '/cache/cache';
+        $views = __DIR__. '/views';
 
-    $app->get('/', function(Request $request, Response $response){
+        $blade = (new Blade($views, $cache))->make($template, $with = []);
+
+        $response->getBody()->write($blade->render());
+
+        return $response;
+    }
+
+    $app->get('/home', function(Request $request, Response $response, $prams){
+
+        return view($response, 'home', [
+            'title' => 'Home Page',
+            'content' => 'Welcome to the Home Page!'
+        ]);
+    });
+
+    $app->get('/', function(Request $request, Response $response, $prams){
         $response->getBody()->write("Hello World");
 
         return $response;
